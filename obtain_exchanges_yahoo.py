faab0904cac9bfe6e86c284fe11d1e10178ceca2
mriera <mrierasagales@gmail.com>
2015-01-23 23:01:47 +0100

    Fichier qui contient le code pour obtenir l'info des differents exchanges et le suffix pour l'ajouter au symbol et constuire le ticker

---
+++ obtain_exchanges_yahoo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+obtain_exchanges_yahoo: code pour obtenir l'information sur 
+les differents exchenges ou marchés financiers.
+On fait l'extraction de la information disponible dans le site web de yahoo:
+http://finance.yahoo.com/exchanges
+"""
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
