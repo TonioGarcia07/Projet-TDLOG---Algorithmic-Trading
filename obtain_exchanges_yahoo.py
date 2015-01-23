83a34215d691f3dcdba7d4e1239cc6663b9634f6
mriera <mrierasagales@gmail.com>
2015-01-23 23:23:28 +0100

    Fonction obtain_exchanges_yahoo qui va chercher l'info sur internet et lestock sur une liste

--- obtain_exchanges_yahoo.py
+++ obtain_exchanges_yahoo.py
@@ -6,7 +6,20 @@ les differents exchenges ou marchés financiers.
 On fait l'extraction de la information disponible dans le site web de yahoo:
 http://finance.yahoo.com/exchanges
 """
+import lxml.html
+import sqlite3
 
+def obtain_exchanges_yahoo():
+    """ Telechargement de la information de Yahoo sur les exchanges """
+   
+    # On utilise xml.html pour télecharger la liste
+    page = lxml.html.parse('http://finance.yahoo.com/exchanges')
+    symbolslist = page.xpath('//table[2]/tr/td/table[1]/tr')[1:]
+    exchanges = []
+    return exchanges 
+            
+if __name__=="__main__":
+    exchanges = obtain_exchanges_yahoo()
 
     
 
