a4a222794c474c8123d471b1da883a61d3cd8aee
mriera <mrierasagales@gmail.com>
2015-01-24 10:54:37 +0100

    modification fonction obtain snp500:extraction symbols

--- obtain_symbols.py
+++ obtain_symbols.py
@@ -12,8 +12,14 @@ def obtain_snp500():
     now = datetime.datetime.utcnow()
     page = lxml.html.parse('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     symbolslist = page.xpath('//table[1]/tr')[1:]
- 	symbols = []
+	symbols = []
 
+	for symbol in symbolslist:
+        tds = symbol.getchildren()   
+        sd = {'symbol': tds[0].getchildren()[0].text,
+              'name':tds[1].getchildren()[0].text,
+              'sector':tds[3].text}
+        symbols.append(('S & P Indices', sd['symbol'],'stock', sd['name'],sd['sector'],'USD',now,now))
     return symbols
 
 if __name__=="__main__":
