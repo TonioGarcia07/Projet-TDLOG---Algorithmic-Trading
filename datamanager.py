90162607537094afd1430d41cd5a2e59cba94d0f
marti <mrierasagales@gmail.com>
2015-01-31 15:06:39 +0100

    Modification de _sql_request pour choisir les dates et rajoute des methodes bar_iterator et next_bar

--- datamanager.py
+++ datamanager.py
@@ -23,17 +23,40 @@ class SQLDataManager(DataManager):
     informations stockées anterieurement et simuler le marché quand on fait un 
     backtest
     """
-    def __init__(self,events,database,ticker_list):
+    def __init__(self,events,database,ticker_list,date_start='',date_end=''):
         self.events = events
         self.database = database
         self.ticker_list = ticker_list
         self.ticker_data = {}
-        self._sql_request()
+        self._sql_request(date_start,date_end)
         
-    def _sql_request(self):
+    def _sql_request(self,date_start,date_end):
+        """
+        Creátion du dictionnaire qui contient les dataframes avec les
+        cotations pour les tickers de la liste entrée dans ticker_list
+        """
         for ticker in self.ticker_list:
             self.ticker_data[ticker] = self.database.toDataframe(self.database.table,self.database.columns_str,ticker)
-
+            self.ticker_data[ticker] = self.ticker_data[ticker][date_start:date_end]
+            
+    def bar_iterator(self,ticker,N=1):
+        """
+        Construction de l'iterateur a partir de la Dataframe du ticker
+        """
+        for bar in self.ticker_data[ticker]:
+            yield tuple([ticker, datetime.strptime(bar[0], '%Y-%m-%d'), 
+                        bar[1][0], bar[1][1], bar[1][2], bar[1][3], bar[1][4]])
+    
+    def next_bar(self):
+        """
+        Prend la suivant bar de l'iterateur et le stock dans le
+        dictionnaire lastest_ticker_data.
+        """
+        for ticker in self.ticker_list:
+            pass
+        
+        
+        
 if __name__=="__main__":
     base ='test.db'
     DailyPrices = DatabaseDailyPrices(base)
@@ -41,5 +64,8 @@ if __name__=="__main__":
     DailyPrices.tickers=[(1,'BNP','.PA')]
     DailyPrices.get_prices()
     DailyPrices.update_prices()
-    DataManager1 = SQLDataManager('events',DailyPrices,['BNP.PA'])
+    DataManager1 = SQLDataManager('events',DailyPrices,['BNP.PA'],datetime(2011, 1, 7),datetime(2012, 1, 7))
     print(DataManager1.ticker_data)
+    print(DataManager1.ticker_data['BNP.PA'].index.dtype)
+    
+    
