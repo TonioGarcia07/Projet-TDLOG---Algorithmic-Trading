10e20f790d3ff67b3ae7aa58d96d380d47c0851d
marti <mrierasagales@gmail.com>
2015-02-06 00:12:47 +0100

    Changement nom: volume ->quantity

--- events.py
+++ events.py
@@ -13,7 +13,7 @@ classe fille4: BookedEvent
 
 class Event:
     """
-    Classe Mère d'où heritent toutes les autres classes Event
+    Classe Mère trivial d'où heritent toutes les autres classes Event
     """
     pass
 
@@ -48,8 +48,8 @@ class TradeEvent(Event):
         self.direction = direction
     
     def __str__(self):
-        return "{} signal generée à {}: {}-{}".format(self.type, 
-                self.datetime, self.direction, self.ticker)
+        return "{} signal {}-{} generée à {}".format(self.type, 
+                self.direction, self.ticker,self.datetime)
 
 class OrderEvent(Event):
     """
@@ -57,7 +57,7 @@ class OrderEvent(Event):
     et qui est la modification du signal TradeEvent pour l'adapté
     au niveau de risque et au portefeuille
     """
-    def __init__(self,ticker,order_type,volume,direction):
+    def __init__(self,ticker,order_type,quantity,direction):
         """
         Parametres:
         ticker: symbol+suffix de l'actif
@@ -68,23 +68,23 @@ class OrderEvent(Event):
         self.type = 'ORDER'
         self.ticker = ticker
         self.order_type = order_type
-        self.volume = volume
+        self.quantity = quantity
         self.direction = direction
     
     def __str__(self):
         return "{} signal generée: {}-{}-{}-{}".format(self.type, 
-                self.direction, self.order_type, self.volume, self.ticker)
+                self.direction, self.order_type, self.quantity, self.ticker)
 
 class BookedEvent(Event):
     """
     Classe fille d'Event qui indique que l'ordre a été executé ou enregistré
     et qui continet les caracteristiques de  l'operation saisie.
     """
-    def __init__(self, datetime, ticker, volume, direction, costs=None):
+    def __init__(self, datetime, ticker, quantity, direction, costs=None):
         self.type = 'BOOKED'
         self.datetime = datetime
         self.ticker = ticker
-        self.volume = volume
+        self.quantity = quantity
         self.direction = direction
         self.calculer_costs(costs)
     
@@ -94,10 +94,10 @@ class BookedEvent(Event):
         competitif)
         """
         if costs is None :
-            if self.volume < 500:
-                self.costs = max(1.3,0.015*self.volume)
+            if self.quantity < 500:
+                self.costs = max(1.3,0.015*self.quantity)
             else:
-                self.costs = max(1.3,0.01*self.volume)
+                self.costs = max(1.3,0.01*self.quantity)
         else: 
             self.costs = costs
 
@@ -107,29 +107,3 @@ class TweetEvent(Event):
 class ImageEvent(Event):
     pass
 
-
-
-
-        
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-        
-    
