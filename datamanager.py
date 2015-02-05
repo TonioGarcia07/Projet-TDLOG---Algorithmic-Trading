86ef0da45c52b13edb7849a368934e6a8e43a768
marti <mrierasagales@gmail.com>
2015-02-05 10:23:56 +0100

    On a enlevé le main et des imports necessaires pour le faire -> on la mis dans test.py

--- datamanager.py
+++ datamanager.py
@@ -7,10 +7,8 @@ a partir des classe Database et les stocker dans un iterateur
 pour 'simuler' le marché quand on fait le backtest
 """
 
-from database import DatabaseDailyPrices
 from datetime import datetime
 from events import DataEvent
-import queue
 
 class DataManager:
     """
@@ -67,51 +65,4 @@ class SQLDataManagerBacktest(DataManager):
         else:
             return bars_list[-N:]
    
-        
-if __name__=="__main__":
-    
-    base ='test.db'
-    ticker ='BNP.PA'
-    
-    DailyPrices = DatabaseDailyPrices(base)
-    DailyPrices.new()
-    DailyPrices.tickers=[(1,'BNP','.PA')]
-    DailyPrices.get_prices()
-    DailyPrices.update_prices()
-    
-    Trivial = queue.Queue()
-    DataManager1 = SQLDataManagerBacktest(Trivial,DailyPrices,[ticker],datetime(2014, 1, 1),datetime(2014, 1, 12))
-    print(DataManager1.ticker_data)
-    print(DataManager1.ticker_data[ticker].index.dtype)
-
-    DataManager1.market()
-    
-    while True:
-        if DataManager1.continue_backtest == True:
-            DataManager1.next_bar()
-        else:
-            break
-        
-        while True:
-            try:
-                event = Trivial.get(False)
-            except:
-                break
-            else:
-                if event is not None:
-                    print(event)
-                    print(DataManager1.get_last_ticker(ticker))
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  
