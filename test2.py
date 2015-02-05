170cc9e0f30a0d690dd9154b78b3676a43d71f5f
marti <mrierasagales@gmail.com>
2015-02-05 10:36:47 +0100

    On a enlevé le main de database avec des test -> on la mis dans test2.py

--- test2.py
+++ test2.py
@@ -1,7 +1,34 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-TDLOG Projet: Algorithmic Trading
-"""
-#test 2
+Test2 : test creation database
 
-#test 3
+"""
+from database import DatabaseExchanges, DatabaseSymbols, DatabaseDailyPrices, DatabaseIntradayPrices
+
+if __name__=="__main__":
+    base ='test.db'
+    Exchange = DatabaseExchanges(base)
+    Exchange.new()
+    Exchange.remplissage()
+    Exchange.affichage()
+    Symbols = DatabaseSymbols(base)
+    Symbols.new()
+    Symbols.remplissage()
+    Symbols.affichage()
+    DailyPrices = DatabaseDailyPrices(base)
+    DailyPrices.new()
+    #DailyPrices.obtain_tickers()
+    DailyPrices.tickers=[(1,'BNP','.PA')]
+    DailyPrices.get_prices()
+    a = DailyPrices.toDataframe(DailyPrices.table, DailyPrices.columns_str,'BNP.PA')
+    print(a.index)
+    #DailyPrices.affichage()
+    #DailyPrices.update_prices()
+    #DailyPrices.affichage()
+    IntradayPrices = DatabaseIntradayPrices(base)
+    IntradayPrices.new()
+    #Intraday.obtain_tickers()
+    IntradayPrices.tickers=[(1,'BNP','.PA')]
+    IntradayPrices.get_prices()
+    IntradayPrices.affichage()
