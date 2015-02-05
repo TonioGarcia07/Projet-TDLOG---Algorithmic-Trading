2627bcf47f5d313588ec27dba055137511eab0f2
marti <mrierasagales@gmail.com>
2015-02-05 20:28:59 +0100

    Class datastorage

---
+++ datastorage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+datastorage.py: classe qui va contenir les differentes infromations generées par
+Strategy and Portfolio
+"""
+
+
+class DataStorage():
+    """
+    
+    """
+    def __init__(self,ticker_list):
+        self.ticker_list = ticker_list
+        self.info = {ticker:{} for ticker in self.ticker_list}
+        self.bar_labels = ['Date','Open','High','Low','Close','Volume','Adj Close']
+        
+    def generate_keys(self,labels_list):
+        for ticker in self.ticker_list:
+            for label in labels_list:
+                self.info[ticker][label]=[]
+    
+    def bar_info_stocking(self,ticker,bar):
+        self.info[ticker]['Date'].append(bar[0][0])
+        for i in range(1,7):
+            self.info[ticker][self.bar_labels[i]].append(bar[0][1][i-1])        
+    
+    def signal_info_stocking(self,ticker,signal):
+        self.info[ticker]['Trade'].append(signal)
+    
+    def info_stocking(self,ticker,information):
+        for (label,value) in information:
+            self.info[ticker][label].append(value)
+
+    
+     
