0850df9b6244338fdbed51fd2025294032db9be1
marti <mrierasagales@gmail.com>
2015-02-05 20:32:55 +0100

    Version 1 class porfolio

---
+++ portfolio.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+portfolio.py: classe qui va contenir le portfolio a gerer et qui va recevoir les
+signals de BUY/SELL de la classe strategy et va determiner le volume selon le
+politique de gestion monaitaire adoptée.
+Va contenir aussi des informations sur les positions ouvertes.
+"""
+
+
